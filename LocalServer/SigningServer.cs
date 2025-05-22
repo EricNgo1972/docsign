914fca2e76b62096c953356ec74dcaa6f1539907
Tung Ngo <ngo-thanh.tung@spc-technology.com>
2025-05-22 14:53:31 -0400

    Update SigningServer.cs

--- LocalServer/SigningServer.cs
+++ LocalServer/SigningServer.cs
@@ -11,7 +11,7 @@ builder.Services.AddCors(options => {
         policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 app.UseCors();
-app.UseCors();
+
 var app = builder.Build();
 
 X509Certificate2 SelectCertificateFromStore()
